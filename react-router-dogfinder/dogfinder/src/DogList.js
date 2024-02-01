3bfd1428e2b1896ddd60a9d947eeab27d27bf0db
Hannah <hcour@gmail.com>
2024-02-01 11:16:51 -0500

    react router dogfinder exercise

---
+++ react-router-dogfinder/dogfinder/src/DogList.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+/**
+ * DogList
+ *
+ * state: none
+ *
+ * props:
+  * dogs: [{name, src}]
+ *
+ * rendered at /dogs
+ *
+ */
+
+const DogList = ({ dogs }) => {
+    return (
+        <div className="DogList">
+            <div className="row mt-4">
+                <div className="col">
+            <h1 className="text-center">Click on dogs for more info.</h1>
+                </div>
+            </div>
+            <div className="row">
+                {dogs.map(d => (
+                    <div className="col-3 text-center" key={d.name}>
+                        <img src={`/${d.src}.jpg`} alt={d.name} />
+                        <h3 className="mt-3">
+                        <Link to={`/dogs/${d.name.toLowerCase()}`}>{d.name}</Link>
+                        </h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default DogList;
